23157b1fc1cb5884adc65fdc17db88389e5aec0b
Alonso Ortega <alonsojortega@gmail.com>
2019-02-09 01:45:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,32 @@
 def consolidate_cart(cart)
-  # code here
+  cart.each_with_object({}) do |item, result|
+    item.each do |type, attributes|
+      if result[type]
+        attributes[:count] += 1
+      else
+        attributes[:count] = 1
+        result[type] = attributes
+      end
+    end
+  end
 end
 
+
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+      name = coupon[:item]
+      if cart[name] && cart[name][:count] >= coupon[:num]
+        if cart["#{name} W/COUPON"]
+          cart["#{name} W/COUPON"][:count] += 1
+        else
+          cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+          cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+        end
+        cart[name][:count] -= coupon[:num]
+      end
+    end
+
+    cart
 end
 
 def apply_clearance(cart)
