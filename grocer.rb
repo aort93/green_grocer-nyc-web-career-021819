b68f463425d08b78f3f523181663ce136be4c51b
Alonso Ortega <alonsojortega@gmail.com>
2019-02-09 03:17:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,7 +41,7 @@ end
 
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolidated_cart, coupons)
+  coupone_price = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   total = 0
   final_cart.each do |name, properties|
